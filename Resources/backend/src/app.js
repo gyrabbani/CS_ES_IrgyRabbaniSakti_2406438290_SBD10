5a47b50b02f335b38fc77f32eb650dbf7cafb58d
gyrabbani <rabbaniirgy@gmail.com>
2026-04-30 21:06:25 +0700

    fix origin di app.js

--- Resources/backend/src/app.js
+++ Resources/backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
